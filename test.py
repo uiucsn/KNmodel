e8b6c9d567d2eeeaa5aa31d12ab365ddaffe9214
Gautham Narayan <gsnarayan@gmail.com>
2018-03-26 22:47:03 -0400

    update test

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ nc = W.NIRCam()
 nc.filter = 'F115W'
 bp = nc._getSynphotBandpass(nc.filter)
 f = at.Table.read('Tables/kilnova_orig_50Mpc_p+5.25.txt', format='ascii')
-spec = S.ArraySpectrum(f['wave_micron'],f['flux_mjy'], waveunits='micron', fluxunits='mjy')
-spec2 = S.ArraySpectrum(f['wave_micron'],f['flux_mjy']*1E5, waveunits='micron', fluxunits='mjy')
+spec = S.ArraySpectrum(f['wave_micron'],f['flux_mjy'], waveunits='micron', fluxunits='ujy')
+spec2 = S.ArraySpectrum(f['wave_micron'],f['flux_mjy']*1E8, waveunits='micron', fluxunits='ujy')
 TELESCOPE_AREA = 25.0 * 10000
 S.setref(area=TELESCOPE_AREA)
 obs = S.Observation(spec, bp,force='taper')
