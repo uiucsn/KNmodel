b97dc1b2dcf40428cfd476d58826cb651cb9dcc7
Deep Chatterjee <deep.chatterjee@ligo.org>
2020-09-16 17:27:38 -0500

    add sun loss, minor bug fixes

--- gw_rates_full.py
+++ gw_rates_full.py
@@ -3,7 +3,7 @@ import io
 import sys
 import os
 from functools import partial
-from urllib import request
+from urllib import parse, request
 
 import numpy as np
 import scipy.stats as spstat
@@ -31,8 +31,13 @@ detector_asd_links = dict(
 )
 
 def get_range(detector):
-    asd_fp = io.BytesIO(
-        request.urlopen(detector_asd_links[detector]).read())
+    psd_url = detector_asd_links[detector]
+    try:
+        # if downloaded locally
+        asd_fp = open(os.path.basename(parse.urlparse(psd_url).path), "rb")
+    except FileNotFoundError:
+        print(f"Downloading PSD for {detector}")
+        asd_fp = io.BytesIO(request.urlopen(psd_url).read())
     freq, asd = np.loadtxt(asd_fp, unpack=True)
     psd = asd**2
     return partial(inspiral_range.range, freq, psd)
@@ -101,6 +106,7 @@ def get_options(argv=None):
     parser.add_argument('--ntry', default=10000, type=int, action=MinZeroAction, help='Set the number of MC samples')
     parser.add_argument('--box_size', default=500., action=MinZeroAction, type=float,\
             help='Specify the side of the box in which to simulate events')
+    parser.add_argument('--sun_loss', default=0.5, help='The fraction not observed due to sun', type=float)
     parser.add_argument('--mean_lograte', default=-5.95, help='specify the lograthim of the mean BNS rate', type=float)
     parser.add_argument('--sig_lograte',  default=0.55, type=float, help='specify the std of the mean BNS rate')
     parser.add_argument('--hdutycycle', default=0.7, action=MinZeroAction, type=float, help='Set the Hanford duty cycle')
@@ -160,7 +166,6 @@ def main(argv=None):
     # setup event rates
     mean_lograte = args.mean_lograte
     sig_lograte  = args.sig_lograte
-    chirp_scale  = args.chirp_scale
     n_try = args.ntry
 
     temp = at.Table.read('kilonova_phottable_40Mpc.txt', format='ascii')
@@ -206,7 +211,7 @@ def main(argv=None):
         maxmag = sss17a - 2.
 
         hmag = temphmag - min(temphmag)
-        hmag[phase <2.5] = 0
+        hmag[phase < 2.5] = 0
         magindex = [(phase - x).argmin() for x in delay]
         magindex = np.array(magindex)
 
@@ -228,47 +233,73 @@ def main(argv=None):
         n_detectors_on = np.array(
             [sum(_) for _ in np.vstack((h_on, l_on, v_on, k_on)).T]
         )
-        dist_ligo_bool  = dist < bns_range_ligo
-        dist_virgo_bool = dist < bns_range_virgo
-        dist_kagra_bool = dist < bns_range_kagra
+        # which detectors observed
+        dist_ligo_bool  = dist <= bns_range_ligo
+        dist_virgo_bool = dist <= bns_range_virgo
+        dist_kagra_bool = dist <= bns_range_kagra
+
+        h_on_and_observed = h_on * dist_ligo_bool
+        l_on_and_observed = l_on * dist_ligo_bool
+        v_on_and_observed = v_on * dist_virgo_bool
+        k_on_and_observed = k_on * dist_kagra_bool
+
+        n_detectors_on_and_obs = np.sum(np.vstack(
+            (h_on_and_observed, l_on_and_observed, v_on_and_observed,
+             k_on_and_observed)).T,
+            axis=1
+        )
+
+        two_det_obs = n_detectors_on_and_obs == 2
+        three_det_obs = n_detectors_on_and_obs == 3
+        four_det_obs = n_detectors_on_and_obs == 4
+
+        # whether this event was not affected by then sun
+        sun_bool = np.random.random(n_events) >= args.sun_loss
+
         distmod = Distance(dist)
         obsmag = absm + distmod.distmod.value
         em_bool = obsmag < 22.
 
-        two_det_bool = n_detectors_on == 2
-        three_det_bool = n_detectors_on == 3
-        four_det_bool = n_detectors_on == 4
-
-        n2_gw_only = np.where(dist_ligo_bool & two_det_bool)[0]
+        n2_gw_only = np.where(two_det_obs)[0]
         n2_gw = len(n2_gw_only)
-        n2_good = np.where(dist_ligo_bool & two_det_bool & em_bool)[0]
+        n2_good = np.where(two_det_obs & sun_bool & em_bool)[0]
         n2 = len(n2_good)
         # sanity check
         assert n2_gw >= n2, "GW events ({}) less than EM follow events ({})".format(n2_gw, n2)
-        n3_gw_only = np.where(dist_ligo_bool & three_det_bool)[0]
+        n3_gw_only = np.where(three_det_obs)[0]
         n3_gw = len(n3_gw_only)
-        n3_good = np.where(dist_virgo_bool & three_det_bool & em_bool)[0]
+        n3_good = np.where(three_det_obs & sun_bool & em_bool)[0]
         n3 = len(n3_good)
         # sanity check
         assert n3_gw >= n3, "GW events ({}) less than EM follow events ({})".format(n3_gw, n3)
-
+        n4_gw_only = np.where(four_det_obs)[0]
+        n4_gw = len(n4_gw_only)
+        n4_good = np.where(four_det_obs & sun_bool & em_bool)[0]
+        n4 = len(n4_good)
+        # sanity check
+        assert n4_gw >= n4, "GW events ({}) less than EM follow events ({})".format(n4_gw, n4)
         return dist[n2_good].value.tolist(), tot_mass[n2_good].tolist(),\
-                dist[n3_good].value.tolist(), tot_mass[n3_good].tolist(),\
-                obsmag[n2_good].tolist(), obsmag[n3_good].tolist(),\
-                n2, n3
+            dist[n3_good].value.tolist(), tot_mass[n3_good].tolist(),\
+            dist[n4_good].value.tolist(), tot_mass[n4_good].tolist(),\
+            obsmag[n2_good].tolist(), obsmag[n3_good].tolist(),\
+            obsmag[n3_good].tolist(), n2, n3, n4
 
     with schwimmbad.SerialPool() as pool:
         values = list(pool.map(dotry, range(n_try)))
     print("Finshed computation, plotting...")
     n_detect2 = []
     n_detect3 = []
+    n_detect4 = []
     dist_detect2 = []
     mass_detect2 = []
     dist_detect3 = []
     mass_detect3 = []
+    dist_detect4 = []
+    mass_detect4 = []
     hmag_detect2 = []
     hmag_detect3 = []
-    for d2, m2, d3, m3,h2, h3,n2, n3 in values:
+    hmag_detect4 = []
+    for d2, m2, d3, m3, d4, m4, h2, h3, h4, n2, n3, n4 in values:
         if n2 >= 0:
             n_detect2.append(n2)
             if n3>0:
@@ -281,10 +312,18 @@ def main(argv=None):
                 dist_detect3 += d3
                 mass_detect3 += m3
                 hmag_detect3 += h3
-
+        if n4>=0:
+            n_detect4.append(n4)
+            if n4 > 0:
+                dist_detect4 += d4
+                mass_detect4 += m4
+                hmag_detect4 += h4
     n_detect2 = np.array(n_detect2)
     n_detect3 = np.array(n_detect3)
+    n_detect4 = np.array(n_detect4)
 
+    #print(f"2 det: {n_detect2};\n3 det: {n_detect3};\n4 det: {n_detect4}")
+    #print(f"2 det mean: {np.mean(n_detect2)};\n3 det mean: {np.mean(n_detect3)};\n4 det mean: {np.mean(n_detect4)}")
     fig_kw = {'figsize':(9.5/0.7, 3.5)}
     fig, axes = plt.subplots(nrows=1, ncols=3, **fig_kw)
 
@@ -295,59 +334,107 @@ def main(argv=None):
     axes[0].hist(n_detect2, histtype='step', \
             bins=ebins, color='C0', lw=3, density=True, zorder=3)
     bin_centers = (ebins[0:-1] + ebins[1:])/2.
-    mean_nevents = np.sum(vals*bin_centers)/np.sum(vals)
-    axes[0].axvline(np.ceil(mean_nevents), color='C0', linestyle='--', lw=1.5, label=r'$\langle N \rangle = {:n}$'.format(np.ceil(mean_nevents)))
+    mean_nevents = np.mean(n_detect2)
+    axes[0].axvline(np.ceil(mean_nevents), color='C0',
+                    linestyle='--', lw=1.5, label=r'$\langle N \rangle = {:n}$'.format(np.ceil(mean_nevents)))
 
-    vals, bins = np.histogram(n_detect3, bins=ebins, density=True)
-    mean_nevents = np.sum(vals*bin_centers)/np.sum(vals)
-    vals*=norm
-    test = dict(zip(ebins, vals))
-    axes[0].hist(list(test.keys()), weights=list(test.values()), histtype='stepfilled', color='C1', alpha=0.5, bins=ebins, zorder=1)
-    axes[0].hist(list(test.keys()), weights=list(test.values()), histtype='step', color='C1', lw=3, bins=ebins, zorder=2)
+    #vals, bins = np.histogram(n_detect3, bins=ebins, density=True)
+    mean_nevents = np.mean(n_detect3)
+    #vals*=norm
+    #test = dict(zip(ebins, vals))
+    #print(ebins, vals)
+    #print("Test")
+    #print(test)
+    axes[0].hist(n_detect3, density=True, histtype='stepfilled', color='C1', alpha=0.5, bins=ebins, zorder=1)
+    axes[0].hist(n_detect3, density=True, histtype='step', color='C1', lw=3, bins=ebins, zorder=2)
+    #axes[0].hist(list(test.keys()), weights=list(test.values()), histtype='stepfilled', color='C1', alpha=0.5, bins=ebins, zorder=1)
+    #axes[0].hist(list(test.keys()), weights=list(test.values()), histtype='step', color='C1', lw=3, bins=ebins, zorder=2)
     axes[0].axvline(np.around(mean_nevents), color='C1', linestyle='--', lw=1.5, label=r'$\langle N \rangle = {:n}$'.format(np.around(mean_nevents)))
     axes[0].legend(frameon=False, fontsize='small')
 
+    #vals, bins = np.histogram(n_detect4, bins=ebins, density=True)
+    mean_nevents = np.mean(n_detect4)
+    #vals*=norm
+    #test = dict(zip(ebins, vals))
+    axes[0].hist(n_detect4, density=True, histtype='stepfilled', color='C2', alpha=0.5, bins=ebins, zorder=1)
+    axes[0].hist(n_detect4, density=True, histtype='step', color='C2', lw=3, bins=ebins, zorder=2)
+    axes[0].axvline(np.around(mean_nevents), color='C2', linestyle='--', lw=1.5, label=r'$\langle N \rangle = {:n}$'.format(np.around(mean_nevents)))
+    axes[0].legend(frameon=False, fontsize='small')
+
     dist_range = np.arange(0, 400., 0.1)
-    kde = spstat.gaussian_kde(dist_detect2, bw_method='scott')
-    pdist = kde(dist_range)
-    axes[1].plot(dist_range, pdist, color='C0', linestyle='-', lw=3, zorder=4)
-    patch1 = axes[1].fill_between(dist_range, np.zeros(len(dist_range)), pdist, color='C0', alpha=0.3, zorder=0)
+    patches = list()
+    legend_text = list()
+    try:
+        kde = spstat.gaussian_kde(dist_detect2, bw_method='scott')
+        pdist = kde(dist_range)
+        axes[1].plot(dist_range, pdist, color='C0', linestyle='-', lw=3, zorder=4)
+        patch1 = axes[1].fill_between(dist_range, np.zeros(len(dist_range)), pdist, color='C0', alpha=0.3, zorder=0)
+        patches.append(patch1)
+        legend_text.append('2 Detector Events')
+        mean_dist = np.mean(dist_detect2)
+        axes[1].axvline(mean_dist, color='C0', linestyle='--', lw=1.5, zorder=6, label=r'$\langle D \rangle = {:.0f}$ Mpc'.format(mean_dist))
+        ind0_40 = dist_range <= 40.
+        ind40_80 = (dist_range <= 100.) & (dist_range > 40.)
+        ind80_160 = (dist_range <= 160.) & (dist_range > 100.)
+        p0_40 = scinteg.trapz(pdist[ind0_40], dist_range[ind0_40])
+        p40_80 = scinteg.trapz(pdist[ind40_80], dist_range[ind40_80])
+        p80_160 = scinteg.trapz(pdist[ind80_160], dist_range[ind80_160])
+        print(p0_40*5, p40_80*5, p80_160*5)
+    except ValueError:
+        print("Could not create KDE since no 2-det detection")
 
-    mean_dist = scinteg.trapz(pdist*dist_range, dist_range)
-    axes[1].axvline(mean_dist, color='C0', linestyle='--', lw=1.5, zorder=6, label=r'$\langle D \rangle = {:.0f}$ Mpc'.format(mean_dist))
+    try:
+        kde = spstat.gaussian_kde(dist_detect3, bw_method='scott')
+        pdist = kde(dist_range)
+        axes[1].plot(dist_range, pdist, color='C1', linestyle='-', lw=3, zorder=2)
+        patch2 = axes[1].fill_between(dist_range, np.zeros(len(dist_range)), pdist, color='C1', alpha=0.5, zorder=1)
+        patches.append(patch2)
+        legend_text.append('3 Detector Events')
+        mean_dist = np.mean(dist_detect3)
+        axes[1].axvline(mean_dist, ymax=0.75, color='C1', linestyle='--', lw=1.5, zorder=6, label=r'$\langle D \rangle = {:.0f}$ Mpc'.format(mean_dist))
+        axes[1].legend(frameon=False, fontsize='small')
+    except ValueError:
+        print("Could not create KDE since no 3-det detection")
 
-    ind0_40 = dist_range <= 40.
-    ind40_80 = (dist_range <= 100.) & (dist_range > 40.)
-    ind80_160 = (dist_range <= 160.) & (dist_range > 100.)
-    p0_40 = scinteg.trapz(pdist[ind0_40], dist_range[ind0_40])
-    p40_80 = scinteg.trapz(pdist[ind40_80], dist_range[ind40_80])
-    p80_160 = scinteg.trapz(pdist[ind80_160], dist_range[ind80_160])
-    print(p0_40*5, p40_80*5, p80_160*5)
+    try:
+        kde = spstat.gaussian_kde(dist_detect4, bw_method='scott')
+        pdist = kde(dist_range)
+        mean_dist = np.mean(dist_detect4)
+        axes[1].plot(dist_range, pdist, color='C2', linestyle='-', lw=3, zorder=2)
+        axes[1].axvline(mean_dist, ymax=0.75, color='C2', linestyle='--', lw=1.5, zorder=6, label=r'$\langle D \rangle = {:.0f}$ Mpc'.format(mean_dist))
+        patch3 = axes[1].fill_between(dist_range, np.zeros(len(dist_range)), pdist, color='C2', alpha=0.5, zorder=1)
+        patches.append(patch3)
+        legend_text.append('4 Detector Events')
+        axes[1].legend(frameon=False, fontsize='small')
+    except ValueError:
+        print("Could not create KDE since no 4-det detection")
 
-    kde = spstat.gaussian_kde(dist_detect3, bw_method='scott')
-    pdist = kde(dist_range)
-    axes[1].plot(dist_range, pdist*norm, color='C1', linestyle='-', lw=3, zorder=2)
-    patch2 = axes[1].fill_between(dist_range, np.zeros(len(dist_range)), pdist*norm, color='C1', alpha=0.5, zorder=1)
-
-    h_range = np.arange(13, 22.5, 0.1)
+    h_range = np.arange(15, 23, 0.1)
     kde = spstat.gaussian_kde(hmag_detect2, bw_method='scott')
     ph = kde(h_range)
     axes[2].plot(h_range, ph, color='C0', linestyle='-', lw=3, zorder=4)
     axes[2].fill_between(h_range, np.zeros(len(h_range)), ph, color='C0', alpha=0.3, zorder=0)
-    mean_h = scinteg.trapz(ph*h_range, h_range)
+    mean_h = np.mean(hmag_detect2)
     axes[2].axvline(mean_h, color='C0', linestyle='--', lw=1.5, zorder=6, label=r'$\langle H \rangle = {:.0f}$ mag'.format(mean_h))
 
     kde = spstat.gaussian_kde(hmag_detect3, bw_method='scott')
     ph = kde(h_range)
-    axes[2].plot(h_range, ph*norm, color='C1', linestyle='-', lw=3, zorder=2)
-    axes[2].fill_between(h_range, np.zeros(len(h_range)), ph*norm, color='C1', alpha=0.5, zorder=1)
-    mean_h = scinteg.trapz(ph*h_range, h_range)
+    axes[2].plot(h_range, ph, color='C1', linestyle='-', lw=3, zorder=2)
+    axes[2].fill_between(h_range, np.zeros(len(h_range)), ph, color='C1', alpha=0.5, zorder=1)
+    mean_h = np.mean(hmag_detect3)
     axes[2].axvline(mean_h, color='C1', linestyle='--', lw=1.5, zorder=6, label=r'$\langle H \rangle = {:.0f}$ mag'.format(mean_h))
     axes[2].legend(frameon=False, fontsize='small')
 
-    mean_dist = scinteg.trapz(pdist*dist_range, dist_range)
-    axes[1].axvline(mean_dist, ymax=0.75, color='C1', linestyle='--', lw=1.5, zorder=6, label=r'$\langle D \rangle = {:.0f}$ Mpc'.format(mean_dist))
-    axes[1].legend(frameon=False, fontsize='small')
+    try:
+        kde = spstat.gaussian_kde(hmag_detect4, bw_method='scott')
+        ph = kde(h_range)
+        axes[2].plot(h_range, ph, color='C2', linestyle='-', lw=3, zorder=2)
+        axes[2].fill_between(h_range, np.zeros(len(h_range)), ph, color='C1', alpha=0.5, zorder=1)
+        mean_h = np.mean(hmag_detect4)
+        axes[2].axvline(mean_h, color='C2', linestyle='--', lw=1.5, zorder=6, label=r'$\langle H \rangle = {:.0f}$ mag'.format(mean_h))
+        axes[2].legend(frameon=False, fontsize='small')
+    except ValueError:
+        print("Could not create KDE for h-mag since no 4 detector events found")
 
     axes[1].set_xlabel('Distance ($D$, Mpc)', fontsize='large')
     axes[1].set_ylabel('$P(D)$', fontsize='large')
@@ -365,10 +452,11 @@ def main(argv=None):
     ymin, ymax = axes[2].get_ylim()
     axes[2].set_ylim(0, ymax)
 
-    fig.legend((patch1, patch2), ('2 Detector Events', '3 Detector Events'), 'upper center', frameon=False, ncol=2, fontsize='medium')
+    fig.legend(patches, legend_text,
+               'upper center', frameon=False, ncol=3, fontsize='medium')
     fig.tight_layout(rect=[0, 0, 1, 0.97], pad=1.05)
-    fig.savefig('gw_detect.pdf')
-    plt.show(fig)
+    fig.savefig(f'gw_detect_{args.mass_distrib}.pdf')
+    plt.show()
 
 
 if __name__=='__main__':
