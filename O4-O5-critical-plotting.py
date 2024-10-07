c037db2fa460c6e096c97a1bf7f2c1d0e2d28091
ved <vedshah.email@gmail.com>
2024-10-07 11:40:09 -0500

    Made improvements to the plots

--- O4-O5-critical-plotting.py
+++ O4-O5-critical-plotting.py
@@ -1,7 +1,11 @@
+import math
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from rates_models import LVK_UG
 
 day_interval = 50
 
@@ -14,8 +18,11 @@ def plot_days_to_KN_discovery_distribution(df, run, color):
 
     run_label = f"O{run}"
 
-
-    bins = np.arange(0, max(data) + day_interval, day_interval)
+    if run == "4":
+        bins = np.arange(0,5 * 365, day_interval)
+        bins = np.append(bins[:-1], 1900)
+    else:
+        bins = np.arange(0, max(data) + day_interval, day_interval)
 
     fig, axs = plt.subplots(ncols=1, nrows=2, figsize=(6, 10))
 
@@ -40,6 +47,7 @@ def plot_days_to_KN_discovery_distribution(df, run, color):
     if run=='4':
 
         axs[1].axvline(744, 0, 1, label="Current O4 Duration", color='black', linestyle='dashed')
+        axs[0].arrow(x=1790, y=0.00008, dx=50, dy=0, head_length=20, width=0.00002, fc ='black')
         axs[1].legend() 
 
     axs[1].grid()
@@ -105,10 +113,10 @@ def rates_vs_days_to_kn(df):
     o4_kn_indices = np.where(o4_days.to_numpy()!=5 * 365)[0]
     o4_no_kn_indices = np.where(o4_days.to_numpy()==5 * 365)[0]
 
-    plt.scatter(rates[o4_kn_indices], o4_days[o4_kn_indices], marker='.', alpha=0.5, label='O4', color='C4')
-    plt.scatter(rates[o4_no_kn_indices], o4_days[o4_no_kn_indices], marker=r'$\uparrow$', s=18, alpha=0.5, label='O4 - No KN', color='C4')
+    plt.scatter(rates[o4_kn_indices], o4_days[o4_kn_indices], marker='.', alpha=0.5, label='O4 - KN detection', color='C4')
+    plt.scatter(rates[o4_no_kn_indices], o4_days[o4_no_kn_indices], marker=r'$\uparrow$', s=18, alpha=0.5, label='O4 - No KN detection', color='C4')
 
-    plt.scatter(rates, o5_days, marker='.', alpha=0.5, label='O5', color='C2')
+    plt.scatter(rates, o5_days, marker='.', alpha=0.5, label='O5 - KN detection', color='C2')
 
     plt.axhline(744, 0, 1, label="Current O4 Duration", color='black', linestyle='dashed')
 
@@ -126,7 +134,14 @@ def rates_vs_delta_days_to_kn(df):
     O4_kn_days, O5_kn_days = df["O4 Days to KN"], df["O5 Days to KN"]
     data = O4_kn_days - O5_kn_days
 
-    plt.scatter(rates, data, marker='.', alpha=0.5, color='gray')
+    o4_days = df[f"O4 Days to KN"]
+    o4_kn_indices = np.where(o4_days.to_numpy()!=5 * 365)[0]
+    o4_no_kn_indices = np.where(o4_days.to_numpy()==5 * 365)[0]
+
+    plt.scatter(rates[o4_kn_indices], data[o4_kn_indices], marker='.', alpha=0.5, color='gray', label='O4 - KN detection')
+    plt.scatter(rates[o4_no_kn_indices], data[o4_no_kn_indices], marker=r'$\uparrow$', alpha=0.5, color='gray', s=18, label='O4 - No  KN detection')
+
+    plt.legend()
 
     plt.xlabel(r"BNS Merger Rate ($Gpc^{-3} \cdot yr^{-1}$)", fontsize='x-large')
     plt.ylabel(r"$\Delta D_{KN}$", fontsize='x-large')
@@ -137,25 +152,28 @@ def rates_vs_delta_days_to_kn(df):
 
 def bns_rates_distribution(df):
 
-    data = df[f"Rates"]
-    data_med = int(np.median(data))
-    data_5 = int(np.percentile(data, 5))
-    data_95 = int(np.percentile(data, 95))
+    rate_exponents = np.arange(1e-3, 1e3, 0.01)
+    rates = 10**rate_exponents
+    samples, d = LVK_UG(n)
 
-    bins = 10**np.linspace(np.log10(min(data)), np.log10(max(data)), 30)
 
-    plt.hist(data, density=True, histtype='step', fill=False, linewidth=2, bins=bins, alpha=1, color="C0")
-    plt.hist(data, density=True, histtype='stepfilled', fill=True, bins=bins, alpha=0.5, color="C0")
+    fx = d.pdf(rate_exponents)
+    Fx = d.cdf(rate_exponents)
+    r_5 = 10**d.ppf(0.05)
+    r_50 = 10**d.ppf(0.50)
+    r_95 = 10**d.ppf(0.95)
+    print(r_5, r_95)
 
-    plt.axvline(data_5, label=r"$P_{5\%=}$" + f"{data_5}", linestyle='dotted', ymin=0, ymax=1, color='black')
-    plt.axvline(data_med, label=r"$P_{50\%=}$" + f"{data_med}", linestyle='dashed', ymin=0, ymax=1, color='black')
-    plt.axvline(data_95, label=r"$P_{95\%=}$" + f"{data_95}", linestyle='dotted', ymin=0, ymax=1, color='black')
-
-    plt.xlabel(r"BNS Merger Rate ($Gpc^{-3} \cdot yr^{-1}$)", fontsize='x-large')
-    plt.ylabel(r"Density", fontsize='x-large')
-
+    plt.plot(rates, fx, color='C0')
+    plt.fill_between(rates, fx, color='C0', alpha=0.5)
+    #plt.plot(rates, Fx, label=r"CDF")
+    plt.axvline(x=r_5, label=rf'$P_{{5\%}}$ = {math.ceil(r_5)}', linestyle='dotted', color='black')
+    plt.axvline(x=r_50, label=rf'$P_{{50\%}}$ = {math.ceil(r_50)}', linestyle='dashed', color='black')
+    plt.axvline(x=r_95, label=rf'$P_{{95\%}}$ = {math.ceil(r_95)}', linestyle='dotted', color='black')
+    plt.xlabel(r'BNS Merger Rate ($GPc^{-3} yr^{-1}$)', fontsize='x-large')
+    plt.ylabel("Density", fontsize='x-large')
     plt.xscale('log')
-    plt.xlim()
+    plt.xlim(left = 10**1.5, right = 10**3.3)
     plt.legend()
 
     plt.savefig("bns_rate_dist.pdf", bbox_inches = "tight")
